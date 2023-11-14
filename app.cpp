46e459553340d5b5db250a3232c0f390bbbe4234
Sigmacci <mamchur.paviel@gmail.com>
2023-11-14 18:49:04 +0100

    Replaced vectors with corresponding structs

--- app.cpp
+++ app.cpp
@@ -2,90 +2,105 @@
 #include <vector>
 #include <cmath>
 
-#define VEHICLE_PARAMS 4
-#define RECEIVER_PARAMS 7
+typedef struct vehicle
+{
+    int capacity;
+    int time;
+    int x_cord;
+    int y_cord;
+} vehicle;
 
-#define VEHICLE_CAPACITY_PARAM 0
-#define VEHICLE_TIME_PARAM 1
-#define VEHICLE_X_CORD_PARAM 2
-#define VEHICLE_Y_CORD_PARAM 3
+typedef struct receiver
+{
+    int id;
+    int x_cord;
+    int y_cord;
+    int demand;
+    int time_window_start;
+    int time_window_end;
+    int service_time;
+} receiver;
 
-#define RECEIVER_ID 0
-#define RECEIVER_X_CORD_PARAM 1
-#define RECEIVER_Y_CORD_PARAM 2
-#define RECEIVER_DEMAND_PARAM 3
-#define RECEIVER_TIME_WINDOW_START_PARAM 4
-#define RECEIVER_TIME_WINDOW_END_PARAM 5
-#define RECEIVER_SERVICE_TIME_PARAM 6
-
-int time_to_arrive(std::vector<int> vehicle, std::vector<int> receiver);
-void heapify(std::vector<std::vector<int>> M, std::vector<double> coefficient, int n, int i);
-void heapsort(std::vector<std::vector<int>> M, std::vector<double> coefficient, int n);
-int greedy(std::vector<std::vector<int>> vehicles, std::vector<std::vector<int>> receivers);
+int time_to_arrive(vehicle v, receiver r);
+void heapify(std::vector<receiver> receivers, std::vector<double> coefficient, int n, int i);
+void heapsort(std::vector<receiver> receivers, std::vector<double> coefficient, int n);
+int greedy(std::vector<vehicle> vehicles, std::vector<receiver> receivers);
 
 int main(int argc, char *argv[])
 {
-    std::vector<std::vector<int>> vehicles;
-    std::vector<std::vector<int>> receivers;
+    std::vector<vehicle> vehicles;
+    std::vector<receiver> receivers;
 }
 
-int time_to_arrive(std::vector<int> vehicle, std::vector<int> receiver)
+int time_to_arrive(vehicle v, receiver r)
 {
     int t = static_cast<int>(sqrt(
-            pow(vehicle[VEHICLE_X_CORD_PARAM] - receiver[RECEIVER_X_CORD_PARAM], 2) +
-            pow(vehicle[VEHICLE_Y_CORD_PARAM] - receiver[RECEIVER_Y_CORD_PARAM], 2)));
+        pow(v.x_cord - r.x_cord, 2) +
+        pow(v.y_cord - r.y_cord, 2)));
 
-    if (vehicle[VEHICLE_TIME_PARAM] + t < receiver[RECEIVER_TIME_WINDOW_START_PARAM])
-        return receiver[RECEIVER_TIME_WINDOW_START_PARAM] - vehicle[VEHICLE_TIME_PARAM];
-    
+    if (v.time + t < r.time_window_start)
+        return r.time_window_start - v.time;
+
     return t;
 }
 
-void heapify(std::vector<std::vector<int>> M, std::vector<double> coefficient, int n, int i)
+void heapify(std::vector<receiver> receivers, std::vector<double> coefficient, int n, int i)
 {
-	int min = i;
-	int left = 2 * i + 1;
-	int right = 2 * i + 2;
-	if (left < n && coefficient[left] < coefficient[min])
-		min = left;
-	if (right < n && coefficient[right] < coefficient[min])
-		min = right;
-	if (min != i)
-	{
-		std::swap(coefficient[i], coefficient[min]);
-		std::swap(M[i], M[min]);
-		heapify(M, coefficient, n, min);
-	}
+    int min = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < n && coefficient[left] < coefficient[min])
+        min = left;
+    if (right < n && coefficient[right] < coefficient[min])
+        min = right;
+    if (min != i)
+    {
+        std::swap(coefficient[i], coefficient[min]);
+        std::swap(receivers[i], receivers[min]);
+        heapify(receivers, coefficient, n, min);
+    }
 }
 
-void heapsort(std::vector<std::vector<int>> M, std::vector<double> coefficient, int n)
+void heapsort(std::vector<receiver> receivers, std::vector<double> coefficient, int n)
 {
-	for (int i = n / 2 - 1; i >= 0; i--)
-	{
-		heapify(M, coefficient, n, i);
-	}
-	for (int i = 1; i < n; i++)
-	{
-		std::swap(coefficient[0], coefficient[n - i]);
-		std::swap(M[0], M[n - i]);
-		heapify(M, coefficient, n - i, 0);
-	}
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(receivers, coefficient, n, i);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        std::swap(coefficient[0], coefficient[n - i]);
+        std::swap(receivers[0], receivers[n - i]);
+        heapify(receivers, coefficient, n - i, 0);
+    }
 }
 
-int greedy(std::vector<std::vector<int>> vehicles, std::vector<std::vector<int>> receivers)
+int greedy(std::vector<vehicle> vehicles, std::vector<receiver> receivers)
 {
     std::vector<double> coefficients;
-    std::vector<std::vector<int>> receivers_copy;
+    std::vector<receiver> receivers_copy;
     std::copy(receivers, receivers_copy, receivers.size());
     int i = 0, j = 0;
     while (i < receivers_copy.size())
     {
         for (int k = 0; k < receivers_copy.size(); k++)
         {
-            coefficients.push_back(time_to_arrive(vehicles[j], receivers_copy[k]) / receivers_copy[k][RECEIVER_DEMAND_PARAM]);
+            coefficients.push_back(time_to_arrive(vehicles[j], receivers_copy[k]) / receivers_copy[k].demand);
         }
         heapsort(receivers_copy, coefficients, receivers_copy.size());
-        if (vehicles[j][VEHICLE_TIME_PARAM] > receivers_copy[i][RECEIVER_TIME_WINDOW_END_PARAM])
+        int t = time_to_arrive(vehicles[j], receivers_copy[i]);
+        if (vehicles[j].capacity >= receivers_copy[i].demand &&
+            vehicles[j].time + t <= receivers_copy[i].time_window_end)
+        {
+            vehicles[j].capacity -= receivers_copy[i].demand;
+            vehicles[j].time += t + receivers_copy[i].service_time;
+            vehicles[j].x_cord = receivers_copy[i].x_cord;
+            vehicles[j].y_cord = receivers_copy[i].y_cord;
+            receivers_copy.erase(receivers_copy.begin() + i);
+            coefficients.clear();
+            i++;
+        }
+        else
         {
             j++;
             if (j == vehicles.size())
@@ -94,23 +109,11 @@ int greedy(std::vector<std::vector<int>> vehicles, std::vector<std::vector<int>>
             }
             continue;
         }
-        int t = time_to_arrive(vehicles[j], receivers_copy[i]);
-        if (vehicles[j][VEHICLE_CAPACITY_PARAM] >= receivers_copy[i][RECEIVER_DEMAND_PARAM] &&
-            vehicles[j][VEHICLE_TIME_PARAM] + t <= receivers_copy[i][RECEIVER_TIME_WINDOW_END_PARAM])
-        {
-            vehicles[j][VEHICLE_CAPACITY_PARAM] -= receivers_copy[i][RECEIVER_DEMAND_PARAM];
-            vehicles[j][VEHICLE_TIME_PARAM] += t + receivers_copy[i][RECEIVER_SERVICE_TIME_PARAM];
-            vehicles[j][VEHICLE_X_CORD_PARAM] = receivers_copy[i][RECEIVER_X_CORD_PARAM];
-            vehicles[j][VEHICLE_Y_CORD_PARAM] = receivers_copy[i][RECEIVER_Y_CORD_PARAM];
-            receivers_copy.erase(receivers_copy.begin() + i);
-            coefficients.clear();
-            i++;
-        }
     }
     int result = 0;
     for (auto vehicle : vehicles)
     {
-        result += vehicle[VEHICLE_TIME_PARAM];
+        result += vehicle.time;
     }
     return result;
 }
